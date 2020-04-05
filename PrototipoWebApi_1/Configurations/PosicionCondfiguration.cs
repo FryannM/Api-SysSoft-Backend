60144384cc30275401de9b71155a63a545e09424
macboockpro <fryann14@hotmail.com>
2020-04-05 04:16:32 -0400

    adding configurations with Fluent api

---
+++ PrototipoWebApi_1/Configurations/PosicionCondfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PrototipoWebApi_1.Modelos;
+
+namespace PrototipoWebApi_1.Configurations
+{
+    public class PosicionCondfiguration : IEntityTypeConfiguration<Posicion>
+    {
+        public void Configure(EntityTypeBuilder<Posicion> builder)
+        {
+            builder.ToTable("Posicion")
+                .HasKey(x => x.Pos_I_Codigo);
+
+            builder.Property(x => x.Pos_I_Codigo)
+                .HasColumnName("Pos_I_Codigo");
+
+            builder.Property(x => x.Pos_V_Descripcion)
+               .HasColumnName("Pos_V_Descripcion");
+        }
+    }
+}   
---
+++ PrototipoWebApi_1/Configurations/ProyectosConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PrototipoWebApi_1.Repositorios;
+
+namespace PrototipoWebApi_1.Configurations
+{
+    public class ProyectosConfigurations : IEntityTypeConfiguration<Proyecto>
+    {
+       
+        public void Configure(EntityTypeBuilder<Proyecto> builder)
+        {
+            builder.ToTable("Proyectos")
+                .HasKey(x => x.Pro_I_Codigo);
+
+            builder.Property(x => x.Pro_I_Codigo)
+                .HasColumnName("Pro_I_Codigo");
+
+            builder.Property(x => x.Pro_V_Descripcion)
+                .HasColumnName("Pro_V_Descripcion");
+
+
+        }
+    }
+}
